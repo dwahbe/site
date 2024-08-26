107f97755ac536f2c8ad148e1180e7c7d03822fc
Dylan Wahbe <dylan@hackclub.com>
2024-08-26 17:38:55 -0400

    Add summary to blog posts
    * Changed where date goes, and date formatting

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -21,14 +21,13 @@ export function BlogPosts() {
             className="flex flex-col space-y-1 mb-4"
             href={`/blog/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
-                {formatDate(post.metadata.publishedAt, false)}
-              </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                {post.metadata.title}
-              </p>
-            </div>
+            <h2 className="text-neutral-900 dark:text-neutral-100 tracking-tight font-medium">
+              {post.metadata.title}
+            </h2>
+            <p className="text-neutral-600 text-sm">{post.metadata.summary}</p>
+            <p className="font-light text-sm text-neutral-400 dark:text-neutral-400 w-[100px] tabular-nums">
+              {formatDate(post.metadata.publishedAt, false)}
+            </p>
           </Link>
         ))}
     </div>
