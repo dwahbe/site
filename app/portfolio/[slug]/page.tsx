833123592c82d526b83ba57eccafa7a73c2ce55d
Dylan Wahbe <dylan@hackclub.com>
2024-08-27 00:13:26 -0400

    got rid of "publishedAt"

--- app/portfolio/[slug]/page.tsx
+++ app/portfolio/[slug]/page.tsx
@@ -19,7 +19,7 @@ export function generateMetadata({ params }) {
 
   let {
     title,
-    publishedAt: publishedTime,
+
     summary: description,
     image,
   } = post.metadata
@@ -34,8 +34,8 @@ export function generateMetadata({ params }) {
       title,
       description,
       type: 'article',
-      publishedTime,
-      url: `${baseUrl}/blog/${post.slug}`,
+
+      url: `${baseUrl}/portfolio/${post.slug}`,
       images: [
         {
           url: ogImage,
@@ -60,34 +60,8 @@ export default function Blog({ params }) {
 
   return (
     <section>
-      <script
-        type="application/ld+json"
-        suppressHydrationWarning
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            '@context': 'https://schema.org',
-            '@type': 'BlogPosting',
-            headline: post.metadata.title,
-            datePublished: post.metadata.publishedAt,
-            dateModified: post.metadata.publishedAt,
-            description: post.metadata.summary,
-            image: post.metadata.image
-              ? `${baseUrl}${post.metadata.image}`
-              : `/og?title=${encodeURIComponent(post.metadata.title)}`,
-            url: `${baseUrl}/blog/${post.slug}`,
-            author: {
-              '@type': 'Person',
-              name: 'My Portfolio',
-            },
-          }),
-        }}
-      />
       <h1 className="title text-4xl">{post.metadata.title}</h1>
-      <div className="flex justify-between items-center mt-2 mb-8 text-sm">
-        <p className="text-sm text-neutral-600 dark:text-neutral-400">
-          {formatDate(post.metadata.publishedAt)}
-        </p>
-      </div>
+      <div className="flex justify-between items-center mt-2 mb-8 text-sm"></div>
       <article className="prose">
         <CustomMDX source={post.content} />
       </article>
