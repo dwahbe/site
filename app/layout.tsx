af52b31096605014d1072e5f9a6e9c516fc43d7a
Dylan Wahbe <dylan@hackclub.com>
2024-08-27 00:05:30 -0400

    Added image

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { Instrument_Sans, Instrument_Serif } from 'next/font/google'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Dylan Wahbe',
+    template: '%s | Dylan Wahbe',
   },
   description: 'This is my portfolio.',
   openGraph: {
@@ -59,14 +59,14 @@ export default function RootLayout({
     <html
       lang="en"
       className={cx(
-        'text-black bg-white dark:text-white dark:bg-black font-sans',
+        'text-black bg-white dark:text-white dark:bg-neutral-900 font-sans',
         sans.variable,
         sans.className,
         serif.variable
       )}
     >
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+        <main className="flex-auto min-w-1 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
           {children}
           <Footer />
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 import { PortfolioPosts } from 'app/components/posts'
+import Image from 'next/image'
+import Portrait from './profilephoto.jpg'
 
 export default function Page() {
   return (
     <section>
+      <Image
+        className="rounded-2xl max-w-md"
+        src={Portrait}
+        alt="Portrait of Dylan Wahbe"
+      />
       <h1 className="mb-8 text-4xl">Hi, I'm Dylan Wahbe</h1>
       <p className="mb-4">hello_world</p>
       <div className="my-8">
