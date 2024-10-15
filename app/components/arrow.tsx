41ef8c7eb71c4e40a373e36f861a4d1bee193692
Dylan Wahbe <dylan@hackclub.com>
2024-10-14 22:58:32 -0400

    increased acessibility on screen readers

--- app/components/arrow.tsx
+++ app/components/arrow.tsx
@@ -6,6 +6,7 @@ export default function ArrowIcon(props) {
       viewBox="0 0 12 12"
       fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
+      aria-hidden
       {...props}
     >
       <path d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z" />
