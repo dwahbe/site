f42276a462077f70cdc269a37847f7dce0362092
Dylan Wahbe <dylan@hackclub.com>
2025-05-14 14:43:08 -0400

    Fixed portfolio posts

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,15 +1,18 @@
+import { getPortfolioPosts } from 'app/portfolio/utils'
 import { PortfolioPosts } from 'app/components/posts'
 
 export const metadata = {
-  title: 'Blog',
-  description: 'Read my blog.',
+  title: 'Portfolio',
+  description: 'Explore my work and projects.',
 }
 
 export default function Page() {
+  const allPosts = getPortfolioPosts()
+
   return (
     <section>
       <h1 className="text-4xl md:text-5xl mb-8">Portfolio</h1>
-      <PortfolioPosts showSummary={true} />
+      <PortfolioPosts posts={allPosts} />
     </section>
   )
 }
